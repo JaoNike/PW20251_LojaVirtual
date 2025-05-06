34061baca08a10b39f0a225f82ca79b27226cd78
jaoNike <jaogabrie00@gmail.com>
2025-05-05 21:55:27 -0300

    adiciona rota para exibir detalhes do produto e ajusta templates para herança

--- main.py
+++ main.py
@@ -3,12 +3,12 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from babel.numbers import format_currency
 
-from produto_repo import criar_tabela, obter_produtos_por_pagina
+from produto_repo import criar_tabela, obter_produto_por_id, obter_produtos_por_pagina
 
 criar_tabela()
 
 app = FastAPI()
-templates = Jinja2Templates(directory="templates")
+templates = Jinja2Templates(directory="./templates")
 
 def format_currency_br(value, currency='BRL', locale='pt_BR'):
     return format_currency(value, currency, locale=locale)
@@ -19,6 +19,11 @@ templates.env.filters['format_currency_br'] = format_currency_br
 def read_root(request: Request):
     produtos = obter_produtos_por_pagina(12, 0)
     response = templates.TemplateResponse("index.html", {"request": request, "produtos": produtos})
+    return response
+@app.get("/produtos/{id}")
+def read_produto(request: Request, id: int):
+    produtos = obter_produto_por_id(id)
+    response = templates.TemplateResponse("produtos.html", {"request": request, "produto": produtos})
     return response
 
 
